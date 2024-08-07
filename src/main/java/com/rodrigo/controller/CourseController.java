0747448519274e26aa2d6e1300a22281c4d98f1e
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-08-06 23:46:34 -0300

    Connect H2 Database + Get Courses

---
+++ src/main/java/com/rodrigo/controller/CourseController.java
@@ -0,0 +1,26 @@
+package com.rodrigo.controller;
+
+import com.rodrigo.model.Course;
+import com.rodrigo.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/courses")
+
+public class CourseController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> list(){
+        return courseRepository.findAll();
+    }
+
+
+}
