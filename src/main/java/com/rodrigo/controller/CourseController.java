838ccde9ff7f33dc0bbf168251ea8c6eae5d8f3b
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2024-08-14 16:34:13 -0300

    Courses: Find by ID

--- src/main/java/com/rodrigo/controller/CourseController.java
+++ src/main/java/com/rodrigo/controller/CourseController.java
@@ -2,10 +2,11 @@ package com.rodrigo.controller;
 
 import com.rodrigo.model.Course;
 import com.rodrigo.repository.CourseRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +23,17 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
     @PostMapping
-    @ResponseStatus(code=HttpStatus.CREATED)
-    public ResponseEntity<Course> create(@RequestBody Course course){
+    @ResponseStatus(code= HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
         return courseRepository.save(course);
     }
 
